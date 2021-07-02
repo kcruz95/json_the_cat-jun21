91291feae1ef49fba927f066065a11f57081c4e7
kcruz95 <kyle.cruz@hotmail.ca>
2021-07-01 19:35:17 -0700

    breedfetcher.js working on node

---
+++ breedfetcher.js
@@ -0,0 +1,17 @@
+const request = require('request');
+
+const breed = process.argv[2];
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+
+request(url, (error, response, body) => {
+  const data = JSON.parse(body);
+  console.log('breed', breed);
+  // console.log(body);
+  console.log('typeof', typeof body);
+  console.log(data);
+  // console.log(response);
+
+  if (error) {
+    return callback(error);
+  }
+});
