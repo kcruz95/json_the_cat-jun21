0b230b9d95e576770faed1b7cc94445959f6a5fa
kcruz95 <kyle.cruz@hotmail.ca>
2021-07-01 20:20:51 -0700

    cats breedFetcherTest.js describe/it null

--- breedfetcher.js
+++ breedfetcher.js
@@ -3,15 +3,21 @@ const request = require('request');
 const breed = process.argv[2];
 const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
-request(url, (error, response, body) => {
-  const data = JSON.parse(body);
-  console.log('breed', breed);
-  // console.log(body);
-  console.log('typeof', typeof body);
-  console.log(data);
-  // console.log(response);
+const fetchBreedDescription = (breedName, callback) => {
+  request(url, (error, response, body) => {
+    const data = JSON.parse(body);
+    console.log('breed', breed);
+    // console.log(body);
+    // console.log('typeof', typeof body);
+    console.log(data);
+    // console.log(response);
 
-  if (error) {
-    return callback(error);
-  }
-});
+    if (error) {
+      return callback(error);
+    }
+  });
+};
+
+
+
+module.exports = fetchBreedDescription;
